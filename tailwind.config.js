a3870e738c342cd3a73763bfac26f42c87c91943
Fir3fliesss <gheraldymoses5@gmail.com>
2024-12-21 00:12:32 +0700

    chore(public):added more images assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,6 @@ export default {
   ],
   theme: {
     extend: {
-<<<<<<< HEAD
       colors: {
         'primary-light': '#7A7A7A',
         'secondary-light': '#D9D9D9',
@@ -14,11 +13,9 @@ export default {
         'secondary-dark': '#313651',
         'accent-color': '#62929A',
       },
-=======
       fontFamily:{
         poppins: ['Poppins', 'sans-serif']
     }
->>>>>>> e415c44 (create cardComponent)
     },
   },
   plugins: [],
